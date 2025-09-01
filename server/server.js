12dd8fb58867e7f53f698c8fba5461183fb5915d
PetersonC20 <petersoncosta.estudos@gmail.com>
2025-08-31 23:11:20 -0300

    server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,34 @@
+const http = require("http");
+const WebSocket = require("ws");
+
 const PORT = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Quiz WS relay OK\n");
+});
+
+const wss = new WebSocket.Server({ noServer: true });
+
+wss.on("connection", (ws) => {
+  ws.on("message", (msg) => {
+    for (const client of wss.clients) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(msg.toString());
+      }
+    }
+  });
+});
+
+server.on("upgrade", (req, socket, head) => {
+  if (req.url === "/quiz") {
+    wss.handleUpgrade(req, socket, head, (ws) => {
+      wss.emit("connection", ws, req);
+    });
+  } else {
+    socket.destroy();
+  }
+});
 
 server.listen(PORT, () => {
   console.log("WS relay escutando na porta " + PORT);
